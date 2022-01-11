66429ef3a45157450765f8d0195638674697994f
lakshh07 <laksh@Lakshays-MacBook-Pro.local>
2022-01-12 00:18:54 +0530

    Change Designs

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
     interval: 3500,
     breakpoints: {
       768: {
-        perPage: 2,
-        gap: ".7rem",
+        perPage: 1,
+        gap: "0.7rem",
         // height: "6rem",
       },
       425: {
@@ -31,49 +31,4 @@ $(document).ready(function () {
   // });
   splide.mount();
   // new Splide(".splide").mount();
-  $("#lightSlider").lightSlider({
-    item: 3,
-    autoWidth: false,
-    slideMove: 1, // slidemove will be 1 if loop is true
-    slideMargin: 10,
-
-    addClass: "",
-    mode: "slide",
-    useCSS: true,
-    cssEasing: "cubic-bezier(0.25, 0, 0.25, 1)", //'cubic-bezier(0.25, 0, 0.25, 1)',//
-    easing: "cubic-bezier(0.25, 0, 0.25, 1)", //'for jquery animation',////
-
-    pauseOnHover: true,
-
-    speed: 250, //ms'
-    auto: true,
-    loop: true,
-    slideEndAnimation: true,
-    pause: 2000,
-
-    // keyPress: true,
-
-    enableTouch: true,
-    enableDrag: true,
-    freeMove: true,
-    swipeThreshold: 40,
-
-    responsive: [
-      {
-        breakpoint: 800,
-        settings: {
-          item: 3,
-          slideMove: 1,
-          slideMargin: 6,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          item: 2,
-          slideMove: 1,
-        },
-      },
-    ],
-  });
 });
